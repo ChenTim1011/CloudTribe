32dda2cca2bc20f49001e47388883bff0c7541e3
TimChen1011 <110208059@g.nccu.edu.tw>
2024-07-12 17:16:37 +0800

    feat: Add item list component and data source
    The code changes include adding a new file `ItemList.tsx` under the `client/components/ui` directory. This file contains a React functional component `ItemList` that renders a list of items with the ability to add items to a shopping cart. The component uses data from the `data.json` file located in the `database` directory.

---
+++ client/components/ui/ItemList.tsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react';
+import data from '../../../database/data.json';
+
+
+interface Product {
+    category: string;
+    img: string;
+    id: string;
+    name: string;
+    price: number;
+  }
+  
+/**
+ * Renders a list of items with the ability to add items to a shopping cart.
+ */
+const ItemList: React.FC = () => {
+    const [cart, setCart] = useState<Product[]>([]);
+
+    /**
+     * Adds an item to the shopping cart.
+     * @param productId - The ID of the product.
+     * @param productName - The name of the product.
+     * @param productPrice - The price of the product.
+     */
+    const addToCart = (productId: string, productName: string, productPrice: number) => {
+        const quantity = 1; 
+        const product: Product = { id: productId, name: productName, price: productPrice, category: "", img: "" };
+        const existingProductIndex = cart.findIndex(item => item.id === product.id);
+
+        if (existingProductIndex !== -1) {
+            const updatedCart = [...cart];
+            updatedCart[existingProductIndex].price += product.price * quantity;
+            setCart(updatedCart);
+        } else {
+            setCart([...cart, { ...product, price: product.price * quantity }]);
+        }
+
+        alert('商品已加入購物車');
+    };
+
+    return (
+        <div>
+            <h1>商品展示</h1>
+            <div className="groupItems">
+                {data.map(item => (
+                    <div key={item.id} className="item">
+                        <div className="box-img">
+                            <img src={item.img} alt={item.name} />
+                        </div>
+                        <div className="description">{item.name}</div>
+                        <div className="price">售價: ${item.price}</div>
+                        <div className="order">
+                            數量：
+                            <input type="number" id={`quantity-${item.id}`} min="1" defaultValue="1" /><br />
+                            <button onClick={() => addToCart(item.id, item.name, item.price)}>
+                                <i className="fas fa-shopping-cart"></i>購物車
+                            </button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+  
+  export default ItemList;
--- client/components/ui/Sidebar.tsx
+++ client/components/ui/Sidebar.tsx
@@ -6,89 +6,95 @@ type SidebarProps = {
   filterCategory: (category: string) => void;
 };
 
+/**
+ * Sidebar component for displaying categories and subcategories.
+ * @param {Object} props - The component props.
+ * @param {Function} props.filterCategory - The function to filter the category.
+ * @returns {JSX.Element} The rendered Sidebar component.
+ */
 const Sidebar: React.FC<SidebarProps> = ({ filterCategory }) => {
-    const categories = [
-        {
-            name: '蔬菜水果．農特產',
-            subcategories: ['各式蔬菜', '季節水果', '菇菌類'],
-        },
-        {
-            name: '肉品',
-            subcategories: ['牛', '豬', '雞．鴨', '調味肉品', '火鍋肉片'],
-        },
-        {
-            name: '海鮮水產',
-            subcategories: ['加工海鮮', '鮮魚', '蝦類', '魚乾'],
-        },
-        {
-            name: '調味．罐頭湯品．南北貨',
-            subcategories: ['調味品', '抹醬．沾醬', '水果抹醬', '花生．巧克力醬', '罐頭'],
-        },
-        {
-            name: '茶飲．咖啡',
-            subcategories: ['綠茶．烏龍茶．其他茶飲', '紅茶．花茶．水果茶', '咖啡', '奶精', '沖泡飲品'],
-        },
-        {
-            name: '飲品',
-            subcategories: ['機能飲料', '保健養生', '蔬果汁'],
-        },
-        {
-            name: '餅乾．巧克力',
-            subcategories: ['精選餅乾', '鹹餅乾', '甜餅乾', '巧克力'],
-        },
-        {
-            name: '個人清潔',
-            subcategories: [
-                '洗護造型', '卸妝洗顏', '口腔清潔用品', '刮鬍用品', '女性衛生',
-                '衛生棉', '棉條', '保養洗護', '染髮劑．用品', '彩妝．護唇膏', '乳液．防曬．保養'
-            ],
-        },
-        {
-            name: '家庭清潔',
-            subcategories: ['清潔工具', '家用清潔', '衣物洗晾收納', '除濕．香氛', '衛浴用品'],
-        },
-        {
-            name: '廚房用品',
-            subcategories: ['餐廚用品', '保鮮容器', '廚房整理', '料理．烘焙器具'],
-        },
-        {
-            name: '家居用品',
-            subcategories: [
-                '燈泡．燈具', '延長線', '電池', '居家保護用品', '五金材料',
-                'DIY工具', '油漆．粉刷'
-            ],
-        },
-        {
-            name: '露營．車宿',
-            subcategories: ['BBQ烤肉器具', '免洗餐具'],
-        },
-        {
-            name: '其它',
-            subcategories: ['乾淨飲水', '濾水壺．濾芯', '濾心．濾芯', '飲水壺．保溫保冰'],
-        },
-    ];    
+        const categories = [
+                {
+                        name: '蔬菜水果．農特產',
+                        subcategories: ['各式蔬菜', '季節水果', '菇菌類'],
+                },
+                {
+                        name: '肉品',
+                        subcategories: ['牛', '豬', '雞．鴨', '調味肉品', '火鍋肉片'],
+                },
+                {
+                        name: '海鮮水產',
+                        subcategories: ['加工海鮮', '鮮魚', '蝦類', '魚乾'],
+                },
+                {
+                        name: '調味．罐頭湯品．南北貨',
+                        subcategories: ['調味品', '抹醬．沾醬', '水果抹醬', '花生．巧克力醬', '罐頭'],
+                },
+                {
+                        name: '茶飲．咖啡',
+                        subcategories: ['綠茶．烏龍茶．其他茶飲', '紅茶．花茶．水果茶', '咖啡', '奶精', '沖泡飲品'],
+                },
+                {
+                        name: '飲品',
+                        subcategories: ['機能飲料', '保健養生', '蔬果汁'],
+                },
+                {
+                        name: '餅乾．巧克力',
+                        subcategories: ['精選餅乾', '鹹餅乾', '甜餅乾', '巧克力'],
+                },
+                {
+                        name: '個人清潔',
+                        subcategories: [
+                                '洗護造型', '卸妝洗顏', '口腔清潔用品', '刮鬍用品', '女性衛生',
+                                '衛生棉', '棉條', '保養洗護', '染髮劑．用品', '彩妝．護唇膏', '乳液．防曬．保養'
+                        ],
+                },
+                {
+                        name: '家庭清潔',
+                        subcategories: ['清潔工具', '家用清潔', '衣物洗晾收納', '除濕．香氛', '衛浴用品'],
+                },
+                {
+                        name: '廚房用品',
+                        subcategories: ['餐廚用品', '保鮮容器', '廚房整理', '料理．烘焙器具'],
+                },
+                {
+                        name: '家居用品',
+                        subcategories: [
+                                '燈泡．燈具', '延長線', '電池', '居家保護用品', '五金材料',
+                                'DIY工具', '油漆．粉刷'
+                        ],
+                },
+                {
+                        name: '露營．車宿',
+                        subcategories: ['BBQ烤肉器具', '免洗餐具'],
+                },
+                {
+                        name: '其它',
+                        subcategories: ['乾淨飲水', '濾水壺．濾芯', '濾心．濾芯', '飲水壺．保溫保冰'],
+                },
+        ];    
 
-  return (
-    <div className="sidebar">
-      <h2>順路經濟購買專區</h2>
-      <ul>
-        {categories.map((category, idx) => (
-          <li key={idx}>
-            <Menu>
-              <MenuButton>{category.name}</MenuButton>
-              <MenuList>
-                {category.subcategories.map((subcategory, subIdx) => (
-                  <MenuItem key={subIdx} onClick={() => filterCategory(subcategory)}>
-                    {subcategory}
-                  </MenuItem>
+    return (
+        <div className="sidebar">
+            <h2>順路經濟購買專區</h2>
+            <ul>
+                {categories.map((category, idx) => (
+                    <li key={idx}>
+                        <Menu>
+                            <MenuButton>{category.name}</MenuButton>
+                            <MenuList>
+                                {category.subcategories.map((subcategory, subIdx) => (
+                                    <MenuItem key={subIdx} onClick={() => filterCategory(subcategory)}>
+                                        {subcategory}
+                                    </MenuItem>
+                                ))}
+                            </MenuList>
+                        </Menu>
+                    </li>
                 ))}
-              </MenuList>
-            </Menu>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+            </ul>
+        </div>
+    );
 };
 
 export default Sidebar;
