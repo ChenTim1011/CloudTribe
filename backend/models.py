87c88553e73ccc0a57d7e0505ebdf2735d757e2d
TimChen1011 <110208059@g.nccu.edu.tw>
2024-07-13 16:24:28 +0800

    feat: Add sqlalchemy Item model for backend

---
+++ backend/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, Float
+from database import Base
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    price = Column(Float)
+    category = Column(String, index=True)
+    image = Column(String)
