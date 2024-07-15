9f2dda22f36c825b0b4479596d8a029d4941e781
TimChen1011 <110208059@g.nccu.edu.tw>
2024-07-15 11:26:21 +0800

    feat: Add http-proxy-middleware for API proxying

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,16 @@
+import { createProxyMiddleware } from "http-proxy-middleware";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:8000/api/:path*", // fastapi server
+      },
+    ];
+  },
+};
 
 export default nextConfig;
