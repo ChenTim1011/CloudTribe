6f2b8556364fa4c9db31f7b6ffaa4d0256cd7ee8
Jasmine <a0905373664@gmail.com>
2024-07-24 15:09:05 +0800

    fix:database.py

--- backend/database.py
+++ backend/database.py
@@ -4,7 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL =  "postgresql://postgres:password@localhost:5432/shopping"
+#DATABASE_URL =  "postgresql://postgres:password@localhost:5432/shopping"
+DATABASE_URL =  "postgresql://postgres:jasmine0108@localhost:5432/postgres"
+
 
 def get_db_connection():
     conn = psycopg2.connect( DATABASE_URL )
