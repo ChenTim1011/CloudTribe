6164e52c98ee4061f0f6eeb2c116910235e5b042
Jasmine <a0905373664@gmail.com>
2024-07-28 21:36:06 +0800

    feat:add models/seller.py file

---
+++ backend/models/seller.py
@@ -0,0 +1,41 @@
+"""
+This module defines the Pydantic models used in the application.
+
+It includes models for UploadImage,.
+These models help in validating and serializing the data exchanged between the API and the database.
+"""
+from pydantic import BaseModel
+class UploadImageRequset(BaseModel):
+    img: str
+    
+class UploadImageResponse(BaseModel):
+    """
+    Model representing an image. 
+    """
+    imgId: str
+    imgLink: str
+class UploadItemRequest(BaseModel):
+    """
+    Model representing upload agricultural product request.
+    """
+    name: str
+    price: str
+    category: str
+    totalQuantity: str
+    offShelfDate: str
+    imgLink: str
+    imgId: str
+    ownerPhone: str
+class ProductBasicInfo(BaseModel):
+    id: str
+    name: str
+    uploadDate: str
+    offShelfDate: str
+
+class ProductInfo(UploadItemRequest):
+    """
+    the class extends UploadItemRequest:
+    Model representing presented agricultural.
+    """
+    id: str
+    uploadDate: str
