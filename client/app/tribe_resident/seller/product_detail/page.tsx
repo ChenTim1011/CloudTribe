cd0bc7ca711ce0b0eb9a8e43847705afcbf985f8
Jasmine <a0905373664@gmail.com>
2024-12-17 12:29:21 +0800

    feat:delete agricultural product

--- client/app/tribe_resident/seller/product_detail/page.tsx
+++ client/app/tribe_resident/seller/product_detail/page.tsx
@@ -6,9 +6,22 @@ import SellerService from '@/services/seller/seller'
 import { ProductInfo } from '@/interfaces/tribe_resident/seller/seller';
 import { Button } from "@/components/ui/button"
 import Link from 'next/link'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { useRouter } from 'next/navigation'
 
 export default function Page(){
   const [productInfo, setProductInfo] = useState<ProductInfo>()
+  const router = useRouter()
   useEffect(()=>{
     if (typeof window !== 'undefined') {
       let product_id = localStorage.getItem('@current_seller_product_id')
@@ -21,12 +34,26 @@ export default function Page(){
     const res = await SellerService.get_product_info(productId)
     setProductInfo(res)
   }
+  const handleDeleteButton = () => {
+    if(productInfo != undefined){
+      try {
+        const res = SellerService.delete_agri_product(productInfo.id)
+        console.log('res:',res)
+      }
+      catch(e){
+        console.log(e)
+      }
+      router.push('/tribe_resident/seller')
+
+    }
+    
+  }
   return(
     <div>
       <NavigationBar/>
       <div className="items-center border-y-2 border-black">
-        
         <div className="flex flex-row justify-center items-center lg:space-x-5 space-x-3 bg-[#E0EBAF] lg:p-8 p-4">
+          
           <img
             src={productInfo?.img_link || '/default-image.png'} 
             alt={productInfo?.name || 'Product Image'} 
@@ -35,16 +62,29 @@ export default function Page(){
             className="lg:w-2/12 w-1/2"
           />
           <div className="flex flex-col text-md lg:text-2xl leading-normal lg:leading-relaxed">
-    
-              <Link href = '/tribe_resident/seller' className="underline">
-                返回上架商品頁面
-              </Link>
-   
+            <Link href = '/tribe_resident/seller' className="underline">
+              返回上架商品頁面
+            </Link>
             <p>商品名稱:{productInfo?.name}</p>
             <p>商品價格:${productInfo?.price.toString()} / {productInfo?.unit}</p>
             <p>商品總數量:{productInfo?.total_quantity.toString()}</p>
             <p>下架日期:{productInfo?.off_shelf_date}</p>
-          
+            {/*<Button className="bg-red-600 w-20" onClick={handleDeleteButton}>刪除商品</Button>*/}
+            <AlertDialog>
+              <AlertDialogTrigger className="bg-red-600 w-28 text-white p-1" >刪除商品</AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>確定要刪除此商品嗎</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    此動作無法復原，請確認後再操作
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel>取消</AlertDialogCancel>
+                  <AlertDialogAction onClick={handleDeleteButton}>刪除</AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
           </div>
         </div>
       </div>
