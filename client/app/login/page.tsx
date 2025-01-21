52bd9b26369bb1598c7d7d4ab9d5273491ad9e26
Jasmine0108 <a0905373664@gmail.com>
2025-01-21 22:12:19 +0800

    fix:remind of clicking above bar after login

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,8 +1,9 @@
 'use client';
 
-import React from 'react';
+import  React from 'react';
 import { NavigationBar } from "@/components/NavigationBar";
 import { UserForm } from '@/components/login/UserForm';
+
 
 const LoginPage: React.FC = () => {
   return (
@@ -17,7 +18,10 @@ const LoginPage: React.FC = () => {
       <div className="w-full">
         <NavigationBar />
       </div>
-      
+      <div className="bg-blue-100 border border-blue-500 text-blue-700 px-4 py-3 rounded relative m-4 p-4" role="alert">
+        <strong className="font-bold">提醒：</strong>
+        <span className="block sm:inline">登入後可點擊上方功能列選擇欲前往的頁面</span>
+      </div>
       <div className="w-full flex justify-start space-x-2 mt-4">
       </div>
       <div className="flex-grow flex items-center justify-center">
